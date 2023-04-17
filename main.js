d51195af1e9f06a39e421c78344a998e07a3e035
Brent Jones <michaeljones081615@gmail.com>
2023-04-16 22:13:44 -0400

    waiting on damin info for js form

--- main.js
+++ main.js
@@ -4,3 +4,21 @@ const naviList = document.getElementById('navi-list');
 toggleButton.addEventListener('click', () => {
     naviList.classList.toggle('active');
 })
+
+function sendEmail() {
+        Email.send({
+            Host : "smtp.gmail.com",
+            Username : "michaeljones081615@gmail.com",
+            Password : "Kentucky22@",
+            To : 'michaeljones081615@gmail.com',
+            From : document.getElementById("email").value,
+            Subject : "New Contact Form Inquiry",
+            Body : "Name: " + document.getElementById("name").value 
+            + "<br> Facility: " + document.getElementById("facilty").value
+            + "<br> Email: " + document.getElementById("email").value
+            + "<br> Phone: " + document.getElementById("phone").value
+            + "<br> Message" + document.getElementById("message").value
+        }).then(
+          message => alert("Message Sent Successfully")
+        );
+}
